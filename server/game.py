6b655ec7712d410a609eff88688bf470ecda03c6
Paweł Marczewski <pwmarcz@gmail.com>
2013-06-30 14:47:34 +0200

    game module: initial

---
+++ server/game.py
@@ -0,0 +1,130 @@
+
+import random
+import unittest
+
+DEBUG = False
+
+TILES = (['M'+str(n) for n in range(1,10)] +
+         ['P'+str(n) for n in range(1,10)] +
+         ['S'+str(n) for n in range(1,10)] +
+         ['X'+str(n) for n in range(1,7)])*4
+
+def next_tile(tile):
+    n = int(tile[1])
+    if tile[0] == 'X':
+        n = n % 7 + 1
+    else:
+        n = n % 9 + 1
+    return tile[0]+str(n)
+
+def dummy_callback(player, msg_type, msg):
+    import pprint
+    pprint.pprint((player, msg_type, msg))
+
+class RuleViolation(Exception):
+    pass
+
+class Game(object):
+    def __init__(self,
+                 nicks=['P1','P2'],
+                 east=0,
+                 callback=dummy_callback):
+        self.callback = callback
+
+        all_tiles = list(TILES)
+
+        if not DEBUG:
+            random.shuffle(all_tiles)
+
+        self.nicks = nicks
+        self.east = east
+
+        # Tiles available for players
+        self.tiles = [all_tiles[:34], all_tiles[34:34*2]]
+
+        self.dora_ind = all_tiles[34*2]
+        self.dora = next_tile(self.dora_ind)
+
+        # Players' hands (None until they've chosen them)
+        self.hand = [None, None]
+
+        for i in range(2):
+            self.callback(i, 'phase_one',
+                          {'nicks': self.nicks,
+                           'tiles': self.tiles[i],
+                           'dora': self.dora,
+                           'you': i,
+                           'east': self.east})
+
+    def on_hand(self, player, hand):
+        if len(hand) != 13:
+            raise RuleViolation
+        if self.hand[player] != None:
+            raise RuleViolation
+        for tile in hand:
+            # this fails if a player doesn't have a tile
+            try:
+                self.tiles[player].remove(tile)
+            except ValueError:
+                raise RuleViolation
+        self.hand[player] = hand
+
+        if self.hand[0] and self.hand[1]:
+            # start the second phase
+            for i in range(2):
+                self.callback(i, 'phase_two', {})
+        else:
+            self.callback(player, 'wait', {})
+
+class GameTestCase(unittest.TestCase):
+    def setUp(self):
+        from collections import deque
+
+        global DEBUG
+        DEBUG = True # suppress randomization
+
+        self.messages = deque()
+
+        self.g = Game(callback=self.callback)
+
+    def assertMessage(self, player, msg_type, msg={}):
+        our_m = self.messages.popleft()
+        self.assertEqual(our_m, (player, msg_type, msg))
+
+    def callback(self, player, msg_type, msg):
+        self.messages.append((player, msg_type, msg))
+
+    def test_init(self):
+        # the game has just been created
+        self.assertMessage(0, 'phase_one',
+                           {'nicks': ['P1', 'P2'],
+                            'tiles': TILES[:34],
+                            'dora': 'M4',
+                            'you': 0,
+                            'east': 0})
+        self.assertMessage(1, 'phase_one',
+                           {'nicks': ['P1', 'P2'],
+                            'tiles': TILES[34:34*2],
+                            'dora': 'M4',
+                            'you': 1,
+                            'east': 0})
+
+    def test_game_scenario(self):
+        self.test_init()
+
+        self.g.on_hand(0, 'M1 M2 M3 M4 M5 M6 M7 M8 M9 P1 P2 P3 P4'.split())
+        self.assertMessage(0, 'wait')
+        self.g.on_hand(1, 'M1 M2 M3 M4 M5 M6 M7 M8 M9 P1 P2 P3 P4'.split())
+        self.assertMessage(0, 'phase_two')
+        self.assertMessage(1, 'phase_two')
+
+    def test_short_hand(self):
+        self.assertRaises(RuleViolation,
+                          lambda: self.g.on_hand(0, 'M1 M2 M3'.split()))
+
+    def test_tiles_outside_initial(self):
+        self.assertRaises(RuleViolation,
+                          lambda: self.g.on_hand(0, ['M1']*13))
+
+if __name__ == '__main__':
+    unittest.main()
