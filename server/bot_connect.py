bf608d49c82aac2854cf57691170ef4c16510b10
Jan Szejko <janek37@gmail.com>
2013-07-14 22:30:33 +0200

    added module for connecting bot to server

---
+++ server/bot_connect.py
@@ -0,0 +1,59 @@
+from socketIO_client import BaseNamespace, SocketIO
+
+from bot import Bot
+
+class MinefieldNamespace(BaseNamespace):
+    def on_wait(self):
+        pass
+
+    def on_phase_one(self, data):
+        tiles = data['tiles']
+        print ' '.join(sorted(tiles))
+        dora_ind = data['dora_ind']
+        print 'dora indicator:', dora_ind
+        east = data['east'] == data['you']
+        options = {
+            'fanpai_winds': ['X1' if east else 'X3'],
+            'dora_ind': dora_ind,
+        }
+        self.me = data['you']
+        self.bot = Bot(tiles=tiles, options=options)
+        tenpai = self.bot.choose_tenpai()
+        print ' '.join(tenpai)
+        self.emit('hand', tenpai)
+
+    def on_wait_for_phase_two(self, data):
+        pass
+
+    def on_phase_two(self, data):
+        pass
+
+    def on_your_move(self, data):
+        tile = self.bot.discard()
+        print tile
+        self.emit('discard', tile)
+
+    def on_discarded(self, data):
+        if data['player'] != self.me:
+            self.bot.opponent_discard(data['tile'])
+
+    def on_ron(self, data):
+        if data['player'] == self.me:
+            print 'I won!'
+            print ' '.join(data['hand'])
+            print ', '.join(data['yaku'])
+            print data['points']
+        else:
+            print 'I lost!'
+
+    def on_draw(self, data):
+        print 'Draw!'
+
+def bot_connect(host, port):
+    socket = SocketIO(host, port)
+    minefield = socket.define(MinefieldNamespace, '/minefield')
+    minefield.emit('hello', 'Bot')
+    socket.wait()
+
+if __name__ == '__main__':
+    bot_connect('localhost', 8080)
