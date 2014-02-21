717caf66795e1517f6d02f1138db01d6a070d87e
Paweł Marczewski <pawel@codility.com>
2014-02-21 23:51:07 +0100

    refactor ui into Ui class

--- server/static/ui.js
+++ server/static/ui.js
@@ -1,31 +1,125 @@
+
+/* jshint undef: true */
+/* global console, alert */
+/* global $ */
+/* global io */
+
+function Ui($elt) {
+    this.$elt = $elt;
+    this.init_elements();
+    this.init_network();
+}
+
+Ui.prototype.find = function(sel) {
+    return this.$elt.find(sel);
+};
+
+Ui.prototype.init_elements = function() {
+    var that = this;
+
+    this.$elt.on('click', '.login button', function() {
+        that.login();
+    });
+
+    this.$elt.on('click', '.tiles .tile', function() {
+        if (this.find('.hand > .tile').length >= 13)
+            return;
+
+        $(this).detach().appendTo('.hand');
+        sort_tiles(this.find('.hand'));
+    });
+
+    this.$elt.on('click', '.hand tile', function() {
+        $(this).detach().appendTo('.tiles');
+        sort_tiles(this.find('.tiles'));
+    });
+
+    this.$elt.on('click', '.submit-hand', function() {
+        that.submit_hand();
+    });
+
+    this.set_status('Enter nick and press Login');
+};
+
+Ui.prototype.init_network = function() {
+    var that = this;
+
+    this.socket = io.connect('/minefield');
+    this.socket.on('phase_one', function(data) {
+        that.set_table_stage_1(data.tiles, data.dora_ind, data.east);
+        console.log('phase_one',data);
+        this.set_status('Choose your hand and press OK');
+    });
+    this.socket.on('wait_for_phase_two', function(data) {
+        this.set_status('Player accepted, waiting for match');
+    });
+};
+
+Ui.prototype.login = function() {
+    var nick = this.find('.login input[name=nick]').val();
+    this.socket.emit('hello', nick);
+
+    this.find('.login').hide();
+
+    this.set_status('Logging in');
+};
+
+Ui.prototype.set_status = function(status) {
+    this.find('.status').text(status);
+};
+
+Ui.prototype.submit_hand = function() {
+    if (this.find(".hand").children().length === 13) {
+        var tiles = [];
+        this.find(".hand").children().each(function() {
+            tiles.push($(this).attr("data-tile"));
+        });
+        this.socket.emit('hand', tiles);
+        this.find('.status').text('Submitting hand...');
+        this.find('.submit-hand').prop('disabled', true);
+    }
+    else {
+        alert("You have to have 13 tiles on hand!");
+    }
+};
+
+Ui.prototype.set_table_stage_1 = function(tiles, dora, east) {
+    this.find('.login').hide();
+    this.find('.table').show();
+
+    this.find(".hand").empty();
+    this.find(".tiles").empty();
+
+    // create tiles & add them to .tiles
+    for (var i=0; i < tiles.length; ++i) {
+        this.find(".tiles").append(create_tile(tiles[i]));
+    }
+    sort_tiles(this.find('.tiles'));
+
+    this.find('.hand').addClass("outlined");
+
+    // TODO: place east
+    this.find(".east-display").append($("<img/>").attr('src', 'tiles/E.svg'));
+
+    this.find(".dora-display").append(create_tile(dora));
+};
+
+Ui.prototype.set_table_stage_2 = function (start)
+{
+    // TODO:
+    // move disposable space & hand to make space for discarded tiles
+    this.find(".hand, .tiles").removeClass("connectedSortable");
+    this.find(".hand").removeClass("outlined");
+    // display discarded tiles
+    // display turn marker
+};
+
 function create_tile(tile_type)
 {
     var newtile = $('<div class="tile"/>');
     newtile.append($("<img/>").attr('src', 'tiles/'+tile_type+'.svg'));
     newtile.attr("data-tile", tile_type);
     return newtile;
-}
-
-function rotate_tile(tile, direction)
-{
-    // TODO:
-    // rotate the tile in place (right, left, up, down?)
-}
-
-function submit_hand()
-{
-    if ($(".hand").children().length === 13) {
-        var tiles = [];
-        $(".hand").children().each(function() {
-            tiles.push($(this).attr("data-tile"));
-        });
-        socket.emit('hand', tiles);
-        $('.status').text('Submitting hand...');
-        $('.submit-hand').prop('disabled', true);
-    }
-    else {
-        alert("You have to have 13 tiles on hand!");
-    }
 }
 
 function sort_tiles(container) {
@@ -40,97 +134,9 @@ function sort_tiles(container) {
     }).appendTo(container);
 }
 
-function set_table_stage_1(tiles, dora, east)
-{
-    $('.login').hide();
-    $('.table').show();
-
-    $(".hand").empty();
-    $(".tiles").empty();
-
-    // create tiles & add them to .tiles
-    for (var i=0; i < tiles.length; ++i) {
-        $(".tiles").append(create_tile(tiles[i]));
-    }
-    sort_tiles($('.tiles'));
-
-    $('.hand').addClass("outlined");
-
-    // TODO: place east
-    $(".east-display").append($("<img/>").attr('src', 'tiles/E.svg'));
-
-    $(".dora-display").append(create_tile(dora));
-
-    $('.tiles').on('click', '.tile', function() {
-        if ($('.hand > .tile').length >= 13)
-            return;
-
-        $(this).detach().appendTo('.hand');
-        sort_tiles($('.hand'));
-    });
-
-    $('.hand').on('click', '.tile', function() {
-        $(this).detach().appendTo('.tiles');
-        sort_tiles($('.tiles'));
-    });
-
-    $('.submit-hand').click(function() {
-        submit_hand();
-    });
-}
-
-function set_table_stage_2(start)
-{
-    // TODO:
-    // move disposable space & hand to make space for discarded tiles
-    $(".hand, .tiles").removeClass("connectedSortable");
-    $(".hand").removeClass("outlined");
-    // display discarded tiles
-    // display turn marker
-}
-
-function discarded(player, tile)
-{
-    // TODO
-}
-
-function your_move()
-{
-    // TODO
-    // enable discarding
-}
-
-function test()
-{
-    set_table_stage_1(['M1', 'M2', 'M3', 'P1', 'P2', 'P3', 'S1', 'S2', 'S3',
-                       'M1', 'M2', 'M3', 'P1', 'P2', 'P3', 'S1', 'S2', 'S3',
-                      ], "M1", true);
-}
-
-function login()
-{
-    var nick = $('.login input[name=nick]').val();
-    socket = connect();
-    socket.emit('hello', nick);
-}
-
-function connect()
-{
-    socket = io.connect('/minefield');
-    socket.on('wait', function() {
-        $('body').removeClass('state-login').addClass('state-waiting');
-    });
-    socket.on('phase_one', function(data) {
-        $('body').removeClass('state-login state-waiting').addClass('state-table');
-        set_table_stage_1(data.tiles, data.dora_ind, data.east);
-        console.log('phase_one',data);
-    });
-    socket.on('wait_for_phase_two', function(data) {
-        $('.status').text('Waiting for opposite player...');
-    });
-    return socket;
-}
-
-$(function() {
-    $('.login button').click(login);
-});
+Ui.prototype.test = function() {
+    this.set_table_stage_1(
+        ['M1', 'M2', 'M3', 'P1', 'P2', 'P3', 'S1', 'S2', 'S3',
+         'M1', 'M2', 'M3', 'P1', 'P2', 'P3', 'S1', 'S2', 'S3',
+        ], "M1", true);
+};
