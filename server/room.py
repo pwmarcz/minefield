bbe6cdea8ebfdeca51a0816f149c9a1840823331
Paweł Marczewski <pawel@codility.com>
2014-08-24 22:08:03 +0200

    New GameServer, not based on Flask

--- server/room.py
+++ server/room.py
@@ -4,48 +4,59 @@ import logging
 
 from game import Game
 
+logger = logging.getLogger('room')
+
 
 class Room(object):
     def __init__(self, players=[None, None], game_class=Game):
         self.game = game_class(callback=self.game_callback)
-        self.players = players
+        self.players = [None, None]
         self.messages = [[], []]
         self.id = 42 # TODO
-        self.logger = logging.getLogger('room.%r' % self.id)
+        self.aborted = False
 
-        self.logger.info('starting')
+        for idx, player in enumerate(players):
+            if player:
+                self.add_player(idx, player)
+
+        logger.info('[game %s] starting', self.id)
         self.game.start()
 
     def game_callback(self, idx, msg_type, msg):
         self.messages[idx].append((msg_type, msg))
         if self.players[idx]:
-            self.logger.info('send to %d: %s %r', idx, msg_type, msg)
+            logger.info('[game %s] send to %d: %s %r', self.id, idx, msg_type, msg)
             self.players[idx].send(msg_type, msg)
 
-    def add_player(self, idx, player):
+    def add_player(self, idx, player, n_received=0):
         assert not self.players[idx]
         self.players[idx] = player
-        self.resend_messages(idx)
+        player.set_room(self, idx)
+        self.resend_messages(idx, n_received)
 
-    def resend_messages(self, idx):
-        n_received = self.players[idx].n_received
+    def remove_player(self, idx):
+        self.players[idx] = None
+
+    def resend_messages(self, idx, n_received):
         messages = self.messages[idx]
         for msg_type, msg in messages[n_received:]:
-            self.logger.info('resend to %d: %s %r', idx, msg_type, msg)
+            logger.info('[game %s] resend to %d: %s %r', self.id, idx, msg_type, msg)
             self.players[idx].send(msg_type, msg)
 
     def send_to_game(self, idx, msg_type, msg):
-        self.logger.info('receive from %d: %s %r', idx, msg_type, msg)
+        logger.info('[game %s] receive from %d: %s %r', self.id, idx, msg_type, msg)
         try:
             handler = getattr(self.game, 'on_'+msg_type)
             handler(idx, msg)
         except:
-            self.logger.exception('exception after receiving')
-            self.shutdown()
+            logger.exception('exception after receiving')
+            self.abort()
 
-    def shutdown(self):
+    def abort(self):
+        self.aborted = True
         for idx in range(2):
-            self.players[idx].shutdown()
+            if self.players[idx]:
+                self.players[idx].shutdown()
 
 
 class RoomTest(unittest.TestCase):
@@ -65,16 +76,22 @@ class RoomTest(unittest.TestCase):
             raise RuntimeError('crashed')
 
     class MockPlayer(object):
-        def __init__(self, n_received=0):
+        def __init__(self):
             self.messages = []
-            self.n_received = n_received
             self.finished = False
+            self.room = None
+            self.idx = None
 
         def send(self, msg_type, msg):
             self.messages.append((msg_type, msg))
 
         def shutdown(self):
             self.finished = True
+
+        def set_room(self, room, idx):
+            self.room = room
+            self.idx = idx
+
 
     def create_room(self, players=[None, None]):
         return Room(players, game_class=self.MockGame)
@@ -104,8 +121,8 @@ class RoomTest(unittest.TestCase):
         room.game.callback(1, 'c', {})
         room.game.callback(1, 'd', {})
         room.game.callback(0, 'e', {})
-        player0 = self.MockPlayer(n_received=1)
-        room.add_player(0, player0)
+        player0 = self.MockPlayer()
+        room.add_player(0, player0, n_received=1)
         self.assertEquals(len(player0.messages), 2)
         self.assertEquals(player0.messages[0][0], 'b')
         self.assertEquals(player0.messages[1][0], 'e')
--- server/server.py
+++ server/server.py
@@ -1,15 +1,10 @@
-import random
 import flask
 import argparse
 from datetime import datetime
 
 import gevent
-import socketio
 import socketio.server
-from socketio.namespace import BaseNamespace
-from socketio.mixins import RoomsMixin, BroadcastMixin
-
-from game import Game
+from game_server import GameServer
 
 
 app = flask.Flask(__name__, static_folder='static', static_url_path='')
@@ -24,114 +19,6 @@ def hello():
 @app.route("/ui.html")
 def ui():
     return flask.redirect('/index.html', 301)
-
-class ServerState():
-    def __init__(self):
-        self.waiting_player = None
-        '''player that currently awaits to be paired'''
-
-        self.game_players = {}
-        '''Game -> (player, player) mapping for socket lookup'''
-
-        self.logger = app.logger
-
-    def add_player(self, player):
-        '''Adds a player to the server.
-        Returns True if a game has started.'''
-
-        if self.waiting_player:
-            players = (self.waiting_player, player)
-            room = GameRoom(players)
-            self.waiting_player = None
-            self.logger.info('[game start] %s vs %s', players[0].nick, players[1].nick)
-            return True
-        else:
-            self.waiting_player = player
-            return False
-
-    def remove_player(self, player):
-        if player == self.waiting_player:
-            self.waiting_player = None
-            player.disconnect()
-        elif player.room:
-            player.room.shutdown()
-        else:
-            player.disconnect()
-
-SERVER = ServerState()
-
-
-class GameRoom(object):
-    def __init__(self, players):
-        self.players = players
-        self.game = Game(nicks=[p.nick for p in players],
-                         callback=self.game_callback)
-        players[0].set_room(self, 0)
-        players[1].set_room(self, 1)
-        self.logger = app.logger
-        self.game.start()
-        self.timer = gevent.spawn(self.run_timer)
-
-    def run_timer(self):
-        start = datetime.now()
-        t = 0
-        while True:
-            new_t = int((datetime.now() - start).seconds)
-
-            if t < new_t:
-                t += 1
-                try:
-                    self.game.beat()
-                except:
-                    self.logger.exception('[%X] exception in Game', id(self))
-                    self.shutdown()
-                    return
-            else:
-                gevent.sleep(seconds=0.5)
-
-    def game_callback(self, idx, msg_type, msg):
-        self.logger.info('[%X] [to %d] %s %r', id(self), idx, msg_type, msg)
-        self.players[idx].emit(msg_type, msg)
-
-    def to_game(self, idx, msg_type, msg):
-        self.logger.info('[%X] [from %d] %s %r', id(self), idx, msg_type, msg)
-        try:
-            getattr(self.game, 'on_'+msg_type)(idx, msg)
-        except:
-            self.logger.exception('[%X] exception in Game', id(self))
-            self.shutdown()
-
-    def shutdown(self):
-        self.timer.kill()
-        for i in xrange(2):
-            self.players[i].disconnect()
-
-
-class MinefieldNamespace(BaseNamespace):
-    def initialize(self):
-        self.logger = app.logger
-        self.nick = ''
-        self.room = None
-        self.idx = None
-
-    def on_hello(self, nick):
-        self.nick = nick
-        if not SERVER.add_player(self):
-            self.emit('wait')
-
-    def set_room(self, room, idx):
-        self.room = room
-        self.idx = idx
-
-    def recv_disconnect(self):
-        self.logger.info("[disconnect] %s", self.nick)
-        SERVER.remove_player(self)
-
-    def on_hand(self, msg):
-        self.room.to_game(self.idx, 'hand', msg)
-
-    def on_discard(self, msg):
-        self.room.to_game(self.idx, 'discard', msg)
 
 
 @app.route('/socket.io/<path:remaining>')
