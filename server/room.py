63adb4c05e8f20fb63c6c622108cceb1d04eb614
Paweł Marczewski <pawel@codility.com>
2014-08-24 21:44:52 +0200

    handle crashes in Game

--- server/room.py
+++ server/room.py
@@ -35,10 +35,17 @@ class Room(object):
             self.players[idx].send(msg_type, msg)
 
     def send_to_game(self, idx, msg_type, msg):
-        handler = getattr(self.game, 'on_'+msg_type)
-        handler(idx, msg)
         self.logger.info('receive from %d: %s %r', idx, msg_type, msg)
+        try:
+            handler = getattr(self.game, 'on_'+msg_type)
+            handler(idx, msg)
+        except:
+            self.logger.exception('exception after receiving')
+            self.shutdown()
 
+    def shutdown(self):
+        for idx in range(2):
+            self.players[idx].shutdown()
 
 
 class RoomTest(unittest.TestCase):
@@ -51,22 +58,35 @@ class RoomTest(unittest.TestCase):
             assert not self.started
             self.started = True
 
+        def on_ping(self, idx, msg):
+            self.callback(1-idx, 'pong', msg)
+
+        def on_crash(self, idx, msg):
+            raise RuntimeError('crashed')
+
     class MockPlayer(object):
         def __init__(self, n_received=0):
             self.messages = []
             self.n_received = n_received
+            self.finished = False
 
         def send(self, msg_type, msg):
             self.messages.append((msg_type, msg))
 
+        def shutdown(self):
+            self.finished = True
+
+    def create_room(self, players=[None, None]):
+        return Room(players, game_class=self.MockGame)
+
     def test_create(self):
-        room = Room(game_class=self.MockGame)
+        room = self.create_room()
         self.assertIsInstance(room.game, self.MockGame)
         self.assertTrue(room.game.started)
 
     def test_send_immediately(self):
         player0 = self.MockPlayer()
-        room = Room([player0, None], game_class=self.MockGame)
+        room = self.create_room([player0, None])
         room.game.callback(0, 'ping_0', {})
         self.assertEquals(len(player0.messages), 1)
         self.assertEquals(player0.messages[0][0], 'ping_0')
@@ -78,7 +98,7 @@ class RoomTest(unittest.TestCase):
         self.assertEquals(player1.messages[0][0], 'ping_1')
 
     def test_resend_after_connect(self):
-        room = Room(game_class=self.MockGame)
+        room = self.create_room()
         room.game.callback(0, 'a', {})
         room.game.callback(0, 'b', {})
         room.game.callback(1, 'c', {})
@@ -90,6 +110,21 @@ class RoomTest(unittest.TestCase):
         self.assertEquals(player0.messages[0][0], 'b')
         self.assertEquals(player0.messages[1][0], 'e')
 
+    def test_send_to_game(self):
+        player0 = self.MockPlayer()
+        player1 = self.MockPlayer()
+        room = self.create_room([player0, player1])
+        room.send_to_game(1, 'ping', {})
+        self.assertEquals(len(player0.messages), 1)
+        self.assertEquals(player0.messages[0][0], 'pong')
+
+    def test_send_and_crash(self):
+        player0 = self.MockPlayer()
+        player1 = self.MockPlayer()
+        room = self.create_room([player0, player1])
+        room.send_to_game(0, 'crash', {})
+        self.assertTrue(player0.finished)
+        self.assertTrue(player1.finished)
 
 if __name__ == '__main__':
     #logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(name)s: %(message)s')
