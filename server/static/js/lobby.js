be3315f9bdf9b56fe311030d884128c2592bc865
Paweł Marczewski <pawel@codility.com>
2014-09-04 21:06:17 +0200

    (WIP) display games list in lobby

--- server/static/js/lobby.js
+++ server/static/js/lobby.js
@@ -25,6 +25,49 @@ function Lobby($elt) {
         self.trigger('login', nick);
     };
 
+    self.update_games = function(data) {
+        function make_row(item) {
+            var $row = $('<tr>');
+            if (item.type == 'game') {
+                $row.append($('<td>').text(item.nick1));
+                $row.append($('<td class="vs">vs</td>'));
+                $row.append($('<td>').text(item.nick2));
+            } else if (item.type == 'player') {
+                $row.append($('<td>').text(item.nick1));
+                $row.append($('<td class="vs">vs</td>'));
+                if (item.is_public)
+                    $row.append($('<td>').append('<button>Challenge</button>'));
+                else
+                    $row.append($('<td>').append('<button disabled>private</button>'));
+            }
+
+            if (!(item.type == 'player' && item.is_public)) {
+                $row.addClass('inactive');
+            }
+            return $row;
+        }
+        var $table = self.find('.games table');
+        // TODO the proper jQuery call
+        $table.html('');
+        $table.append(
+            $('<tr>')
+                .append('<td><button>New game</button></td>')
+                .append('<td class="vs">')
+                .append('<td>')
+        );
+        $.each(data, function(i, item) {
+            self.find('.games table').append(make_row(item));
+        });
+
+    };
+
     self.init();
+
+    self.update_games([
+        {type: 'game', 'nick1': 'Lorem', 'nick2': 'Ipsum'},
+        {type: 'player', 'nick1': 'Lorem'},
+        {type: 'player', 'nick1': 'Lorem', is_public: true},
+        {type: 'player', 'nick1': 'Ipsum', is_public: false},
+    ]);
     return self;
 }
