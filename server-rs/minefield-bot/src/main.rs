c0daf6a2b8353a8e2f491c0903c76f898c066edb
Paweł Marczewski <pwmarcz@gmail.com>
2020-04-04 20:31:10 +0200

    Parse command line args

--- server-rs/minefield-bot/src/main.rs
+++ server-rs/minefield-bot/src/main.rs
@@ -1,3 +1,4 @@
+extern crate clap;
 extern crate env_logger;
 extern crate failure;
 extern crate failure_derive;
@@ -9,9 +10,27 @@ mod bot;
 mod comm;
 mod protocol;
 
+use clap::{App, Arg};
+
 fn main() {
     env_logger::builder()
         .filter_level(log::LevelFilter::Info)
         .init();
-    bot::run_bot("ws://localhost:8080/ws", "RustBot").unwrap();
+
+    let matches = App::new("Minefield Bot")
+        .arg(
+            Arg::with_name("server_url")
+                .long("server-url")
+                .takes_value(true),
+        )
+        .arg(Arg::with_name("nick").long("nick").takes_value(true))
+        .get_matches();
+
+    let server_url = matches
+        .value_of("server_url")
+        .unwrap_or("ws://localhost:8080/ws");
+
+    let nick = matches.value_of("nick").unwrap_or("RustBot");
+
+    bot::run_bot(server_url, nick).unwrap();
 }
